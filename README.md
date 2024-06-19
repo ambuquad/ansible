f7e050064b685922dcc60124ffe5675d085d2f9d
Dennis <ambuquad@protonmail.com>
2024-06-19 14:33:41 +0200

    added nodes

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # ansible-exam prep
 
-Repo for quick setup of your ansible-exam prep lab.
+Repo for quick setup of your ansible-exam(EX-294) prep lab.
 
 Just execute setup.sh
+
+User: student
+Pass: 2stu3dent
+
+## Virt-install nice to know
+
+See OS list                 - virt-install --osinfo list
+Show cloud-init options     - virt-install --cloud-init=?
--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ if [ -f $CloudImage ]; then
    echo "Rocky-9 Cloud image is present"
 else
    echo "Going to get Rocky-9 Cloud image"
-   wget https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2
+   wget https://dl.rockylinux.org/pub/rocky/9/images/x86_64/$CloudImage
 fi
+
+## Setup Contoller node
 
 if [ -f Controller-node.qcow2 ]; then
    virsh destroy Controller-node
@@ -22,5 +24,26 @@ echo `pwd`
 virt-install --name Controller-node \
 --memory 1536 --noreboot \
 --os-variant detect=on,name=rocky9-unknown \
---cloud-init user-data=`pwd`"/data-test/user-data",meta-data=`pwd`"/data-test/meta-data" \
+--noautoconsole \
+--cloud-init user-data=`pwd`"/data-controller/user-data",meta-data=`pwd`"/data-controller/meta-data" \
 --disk=size=10,backing_store=`pwd`"/Controller-node.qcow2"
+
+## Setup Node-01
+
+if [ -f Node-01.qcow2 ]; then
+   virsh destroy Node-01
+   virsh undefine Node-01
+   rm -rf Node-01.qcow2
+fi
+
+echo "Create Node-01.qcow2"
+cp $CloudImage Node-01.qcow2
+
+echo `pwd`
+
+virt-install --name Node-01 \
+--memory 1536 --noreboot \
+--os-variant detect=on,name=rocky9-unknown \
+--noautoconsole \
+--cloud-init user-data=`pwd`"/data-node01/user-data",meta-data=`pwd`"/data-node01/meta-data" \
+--disk=size=10,backing_store=`pwd`"/Node-01.qcow2"
